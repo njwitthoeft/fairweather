48d5ca5301d7a55377478def731d6b824d0fdb63
Nicholas Witthoeft <njwitthoeft@gmail.com>
2026-04-22 15:42:34 -0800

    add agents md, fix imports

--- src/fairweather/main.py
+++ src/fairweather/main.py
@@ -1,26 +1,23 @@
+from fairweather.tides import (
+    TideRequest,
+    fetch_tides,
+    find_next_tides,
+    find_last_tide,
+)
+
+
 def main():
-    print("Hello from fairweather!")
-
-    from src.fairweather.tides import (
-        TideRequest,
-        fetch_tides,
-        find_next_tides,
-        find_last_tide,
-    )
-
     response = fetch_tides(TideRequest())
 
-    for tide in response.predictions:
-        print(tide.local_time())
-
     last_tide = find_last_tide(response.predictions)
-    print("Last tide:")
-    print(last_tide)
-
     next_tides = find_next_tides(response.predictions)
+
+    print("Last tide:")
+    print(last_tide.local_time())
+
     print("Next tides:")
     for tide in next_tides:
-        print(tide)
+        print(tide.local_time())
 
 
 if __name__ == "__main__":
